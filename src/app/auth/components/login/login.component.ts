20bf6304575854c7c458a2b6cdf068f869feed19
juanmac09 <mateoromero0910@gmail.com>
2024-08-31 23:07:04 -0500

    feat(Login):A new version of login with error handling was generated.

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,25 +1,113 @@
 import { Component } from '@angular/core';
-import { RouterLink,Router } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
 import { AuthService } from '../../services/authService/auth-service.service';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink,FormsModule,FormsModule,CommonModule],
+  imports: [RouterLink, FormsModule, FormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-credentials = { email: '', password: '' };
+  credentials = { email: '', password: '' };
+  generalError: string | null = null;
+  emailError: string | null = null;
+  passwordError: string | null = null;
 
   constructor(private authService: AuthService, private router: Router) {}
 
-  login():void{
+  /**
+   * Validate inputs and handle the login process
+   */
+  onSubmit(): void {
+    // Reset errors before validation
+    this.resetErrors();
+
+    // Validate inputs
+    if (this.validateInputs()) {
+      this.login();
+    }
+  }
+
+  /**
+   * Validate the email and password inputs
+   * @returns {boolean} - Returns true if inputs are valid, false otherwise
+   */
+  private validateInputs(): boolean {
+    let isValid = true;
+
+    if (!this.credentials.email) {
+      this.emailError = 'El correo electrónico es obligatorio.';
+      isValid = false;
+    } else if (!this.isValidEmail(this.credentials.email)) {
+      this.emailError = 'Formato de correo electrónico no válido.';
+      isValid = false;
+    }
+
+    if (!this.credentials.password) {
+      this.passwordError = 'La contraseña es obligatoria.';
+      isValid = false;
+    }
+
+    return isValid;
+  }
+
+  /**
+   * Validate the format of the email address
+   * @param email - The email address to validate
+   * @returns {boolean} - Returns true if the email format is valid, false otherwise
+   */
+  private isValidEmail(email: string): boolean {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  }
+
+  /**
+   * Function to handle user login.
+   */
+  private login(): void {
     this.authService.login(this.credentials).subscribe({
       next: () => this.router.navigate(['/dashboard']),
-      error: (error) => console.error('Error:', error)
+      error: (error) => this.handleError(error),
     });
+  }
+
+  /**
+   * Reset all error messages
+   */
+  private resetErrors(): void {
+    this.generalError = null;
+    this.emailError = null;
+    this.passwordError = null;
+  }
+
+  /**
+   * Handle the error response and display the appropriate message
+   * @param error - the error response from the server
+   */
+  private handleError(error: any): void {
+    if (error.status === 401) {
+      const errorData = error.error;
+
+      if (errorData.title === 'Unauthorized' && errorData.status === 401) {
+        if (errorData.detail === 'Bad credentials') {
+          this.generalError =
+            'El correo electronico o la contraseña son incorrectos.';
+        } else {
+          this.generalError =
+            errorData.description ||
+            'Datos inválidos. Por favor, verifique su información.';
+        }
+      } else {
+        this.generalError =
+          'Datos inválidos. Por favor, verifique su información.';
+      }
+    } else {
+      this.generalError =
+        'Error inesperado. Por favor, intente de nuevo más tarde.';
+    }
   }
 }
--- src/app/auth/services/authService/auth-service.service.ts
+++ src/app/auth/services/authService/auth-service.service.ts
@@ -3,32 +3,53 @@ import { Observable, tap } from 'rxjs';
 import { RequestHttpService } from '../../../http/request-http.service';
 import { Router } from '@angular/router';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  private authEndpoint = '/auth/login'; 
+  private authEndpoint = '/auth/login';
   private tokenKey = 'authToken';
-  constructor(private requestHttpService: RequestHttpService, private router: Router) {}
+  constructor(
+    private requestHttpService: RequestHttpService,
+    private router: Router
+  ) {}
 
-  login(credencials:{email: string, password: string}):Observable<any>{
+  /**
+   * This function sends a POST request to the server with the provided credentials.
+   * If the response contains a token, it logs the token to the console and sets it in the local storage.
+   * @param {Object} credencials - An object containing the user's email and password.
+   * @returns {Observable<any>} - An observable that emits the response from the server.
+   */
+  login(credencials: { email: string; password: string }): Observable<any> {
 
-    return this.requestHttpService.postData(this.authEndpoint,credencials).pipe(
-      tap(response => {
-        if (response.token) {
-          console.log(response.token);
-          this.setToken(response.token);
-        }
-      })
-    )
+    return this.requestHttpService
+      .postData(this.authEndpoint, credencials)
+      .pipe(
+
+        tap((response) => {
+          if (response.token) {
+            this.setToken(response.token);
+          }
+        })
+      );
   }
 
-  private setToken(token:string):void{
+  /**
+   * This function sets the provided token in the local storage using the key specified in the tokenKey property.
+   * @param {string} token - The token to be stored in the local storage.
+   */
+  private setToken(token: string): void {
+
     localStorage.setItem(this.tokenKey, token);
   }
 
-
+  /**
+   * This private function retrieves the token from the local storage using the key specified in the tokenKey property.
+   * It first checks if the window object is defined, which indicates that the code is running in a browser environment.
+   * If the window object is defined, it uses the getItem method of the localStorage object to retrieve the token with the key specified in the tokenKey property.
+   * If the token is found, it is returned; otherwise, null is returned.
+   * @returns {string | null} - The token retrieved from the local storage, or null if the token is not found.
+   */
   private getToken(): string | null {
     if (typeof window !== 'undefined') {
       return localStorage.getItem(this.tokenKey);
@@ -36,22 +57,31 @@ export class AuthService {
     return null;
   }
 
-
+  /**
+   * This function checks if the user is authenticated.
+   * It retrieves the token from the local storage using the key specified in the tokenKey property.
+   * If the token is found, it decodes the token and verifies its expiration time.
+   * If the token is expired, the function returns false, indicating that the user is not authenticated.
+   * Otherwise, the function returns true, indicating that the user is authenticated.
+   * @returns {boolean} - True if the user is authenticated, false otherwise.
+   */
   isAuthenticated(): boolean {
-
     const token = this.getToken();
     if (!token) {
-        return false;
+      return false;
     }
 
-    const payload = JSON.parse(atob(token.split(".")[1]));
+    const payload = JSON.parse(atob(token.split('.')[1]));
     const exp = payload.exp * 1000;
-    return Date.now()< exp;
-
+    return Date.now() < exp;
   }
 
-
-
+  /**
+   * This function logs the user out by removing the token from the local storage and redirecting the user to the login page.
+   * It uses the removeItem method of the localStorage object to remove the token with the key specified in the tokenKey property.
+   * After removing the token, it uses the navigate method of the Router object to redirect the user to the login page.
+   * @returns {void} - This function does not return any value; it only performs the actions of logging the user out.
+   */
   logOut(): void {
     localStorage.removeItem(this.tokenKey);
     this.router.navigate(['/login']);
