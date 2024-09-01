7bb89145d56a98db59309b5e6d18c9d82e89da17
juanmac09 <mateoromero0910@gmail.com>
2024-09-01 00:49:19 -0500

    feat(projects): Added functionality to create projects.

--- src/app/projects/components/create-projects/create-projects.component.ts
+++ src/app/projects/components/create-projects/create-projects.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CreateProjectService } from '../../services/create-project/create-project.service';
 import { Router } from '@angular/router';
 
@@ -9,14 +14,18 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './create-projects.component.html',
-  styleUrls: ['./create-projects.component.css']
+  styleUrls: ['./create-projects.component.css'],
 })
 export class CreateProjectsComponent {
   projectForm: FormGroup;
   successMessage: string | null = null;
   errorMessage: string | null = null;
 
-  constructor(private fb: FormBuilder, private projectCreateService: CreateProjectService, private router: Router) {
+  constructor(
+    private fb: FormBuilder,
+    private projectCreateService: CreateProjectService,
+    private router: Router
+  ) {
     this.projectForm = this.fb.group({
       projectName: ['', [Validators.required, Validators.minLength(3)]],
       projectDescription: ['', [Validators.required, Validators.minLength(8)]],
@@ -31,6 +40,15 @@ export class CreateProjectsComponent {
     return this.projectForm.get('projectDescription')!;
   }
 
+  /**
+   * onSubmit: This method is responsible for handling the form submission when the user creates a new project.
+   * It first checks if the form is invalid. If it is, it marks all fields as touched and returns early.
+   * If the form is valid, it formats the project data and sends it to the `projectCreateService` for creation.
+   * Upon successful creation, it sets a success message, clears the error message, resets the form, and hides the success message after 3 seconds.
+   * If there's an error during the creation process, it sets an error message and clears the success message.
+   * @param None
+   * @returns None
+   */
   onSubmit(): void {
     if (this.projectForm.invalid) {
       this.projectForm.markAllAsTouched();
@@ -43,16 +61,23 @@ export class CreateProjectsComponent {
         this.successMessage = 'Proyecto creado exitosamente';
         this.errorMessage = null;
         this.projectForm.reset();
-        setTimeout(() => this.successMessage = null, 3000); // Ocultar mensaje después de 3 segundos
+        setTimeout(() => (this.successMessage = null), 3000); // Ocultar mensaje después de 3 segundos
       },
       error: () => {
         this.successMessage = null;
-        this.errorMessage = 'Error al crear el proyecto. Por favor, intente más tarde.';
-      }
+        this.errorMessage =
+          'Error al crear el proyecto. Por favor, intente más tarde.';
+      },
     });
   }
 
-  formatData(projectData: any) {
+  /**
+   * formatData: This method formats the project data before sending it to the `projectCreateService` for creation.
+   * It takes the project data as input and returns an object with the formatted name and description.
+   * @param projectData: The project data object containing the project name and description.
+   * @returns A formatted object with the project name and description.
+   */
+  formatData(projectData: any): { name: string; description: string } {
     return {
       name: projectData.projectName,
       description: projectData.projectDescription,
