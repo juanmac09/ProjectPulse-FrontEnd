bb98037aba2766f34edcc836b89080c92d9754db
juanmac09 <mateoromero0910@gmail.com>
2024-09-01 23:09:37 -0500

    fix(configs): Added functionality to upload to production

--- src/app/http/request-http.service.ts
+++ src/app/http/request-http.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestHttpService {
-  private apiUrl = "http://localhost:8080";
+  private apiUrl = environment.API_URL;
   private headers = new HttpHeaders({
     'Content-Type': 'application/json',
   });
